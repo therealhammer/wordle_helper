af8db3437ad2fc06fb76cd13c0dd13b544e6573b
Thierry Hammer <freddymaerz@googlemail.com>
2022-03-19 13:40:01 +0100

    Use python spell library instead of aspell and make it 10000 times faster (literally)

--- wordle.py
+++ wordle.py
@@ -5,45 +5,64 @@ import os
 import string
 import sys
 import argparse
+from spellchecker import SpellChecker
+
+possibles = []
 
 def main(argv):
+  # Get all necesary arguments
   parser = argparse.ArgumentParser()
   parser.add_argument('-g', '--grey_letters', help= "Known grey letters that are defenitely not in the word", default="")
   parser.add_argument('-y', '--yellow_letters', help="Known yellow letters that are definitely in the word", default="")
   parser.add_argument('-l', '--language', help="Language: en|de", default="en")
   parser.add_argument('-w', '--wordl', help="Wordle word with known greens and _ for unknowns", required=True)
   args = vars(parser.parse_args())
-  iterate(args["wordl"], args["yellow_letters"], args["grey_letters"], args["language"])
+
+  spell = SpellChecker(language=args["language"])
+  iterate(args["wordl"], args["yellow_letters"], args["grey_letters"])
+  for i in possibles:
+    isaword(i, spell)
   
-def iterate(word, y, g, l):
+# Make a list of all posssible letter combinations
+def iterate(word, y, g):
   for i in word:
     if i == "_":
       for j in string.ascii_lowercase:
         newword = word.split("_", 1)[0] + j + word.split("_", 1)[1]
-        iterate(newword, y, g, l)
-        check(newword, y, g, l)
+        iterate(newword, y, g)
+        if check(newword, y, g):
+          possibles.append(newword)
       break
 
-def check(word, y, g, l):
+# Check if word complies with grey and yellow letters
+def check(word, y, g):
   valid = False
-  for i in y:
-    if i in word:
-      valid = True
-  if y == "":
+  # Word has all yellow letters
+  if set(y).issubset(word): 
     valid = True
+  # Word contains not one of the grey letters
   for i in word:
     if i in g:
       valid = False
+  # Word is from final iteration without any blanks
   if "_" in word:
     valid = False
-  if valid:
-    ret = subprocess.getoutput("echo " + word + " | aspell -d " + l + " pipe")
+  return valid
+
+# Check with aspell if word exists (Old, slow, depricated)
+def isaword_aspell(word, l):
+  ret = subprocess.getoutput("echo " + word + " | aspell -d " + l + " pipe")
+  if ret.find("*") > 0:
+    print("Valid word: " + word)
+  if l is not "en":
+    ret = subprocess.getoutput("echo " + word.title() + " | aspell -d " + l + " pipe")
     if ret.find("*") > 0:
       print("Valid word: " + word)
-    if l is not "en":
-      ret = subprocess.getoutput("echo " + word.title() + " | aspell -d " + l + " pipe")
-      if ret.find("*") > 0:
-        print("Valid word: " + word)
 
+# Check with dict if word exists
+def isaword(word, spell):
+  if spell.known([word]):
+    print("Valid word: " + word)
+ 
 if __name__ == "__main__":
    main(sys.argv[1:])
